b98ee94843b665d8a33c5f3688e3c56bcad8f48b
Wade Tandy <wade@wadetandy.com>
2015-01-06 00:52:14 -0500

    Setup basic angular application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,7 @@
 //= require jquery/dist/jquery
 //= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
+//= require angular/angular
+//= require angular-route/angular-route
+//= require angular-resource/angular-resource
+
+angular.module('ngCrud', ['ng', 'ngResource', 'ngRoute'])
