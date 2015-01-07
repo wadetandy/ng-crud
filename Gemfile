10132f2e1d2784f3c77cfcefcf61bc3a4c21d94d
Wade Tandy <wade@wadetandy.com>
2015-01-07 02:57:41 -0500

    Convert basic post controller scaffold to simple JSON CRUD API

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml'
+gem 'responders', '~> 2.0'
 gem 'active_model_serializers'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,57 +1,43 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  respond_to :json
 
-  # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    respond_with Post.all
   end
 
-  # GET /posts/1
   # GET /posts/1.json
   def show
+    respond_with Post.find(params[:id])
   end
 
   # GET /posts/new
   def new
-    @post = Post.new
+    respond_with Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if post.save
+      respond_with post, status: :created
+    else
+      format.json { render json: post.errors, status: :unprocessable_entity }
     end
   end
 
-  # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    post = Post.find(params[:id])
+
+    if post.update(post_params)
+      respond_with post, status: :ok
+    else
+      render json: post.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
@@ -62,13 +48,9 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :body, :published)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :body, :published)
+  end
 end
