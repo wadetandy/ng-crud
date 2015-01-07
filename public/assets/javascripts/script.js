b041626f10368e1ca61622edddc65743312b2882
richmolj <richmolj@gmail.com>
2015-01-07 17:30:55 -0500

    Add step 1

--- public/assets/javascripts/script.js
+++ public/assets/javascripts/script.js
@@ -1,1 +1,19 @@
 app = angular.module('ngBlog', ['ngResource', 'ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/posts/new',
+      {
+        templateUrl: '/assets/templates/posts/form.html',
+        controller: 'postFormCtrl',
+        controllerAs: 'ctrl',
+        reloadOnSearch: false
+      }
+    )
+});
+
+app.config(function($locationProvider) { $locationProvider.hashPrefix('!')})
+
+app.controller('postFormCtrl', function($scope) {
+  // ... todo ...
+})
